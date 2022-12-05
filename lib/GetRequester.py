aee7a4cb4b62911549a86def75101fc85b3445fa
Prabhdip Gill <prabhdip.gill@flatironschool.com>
2022-12-05 00:54:16 -0800

    Add solution branch

---
+++ lib/GetRequester.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+class GetRequester:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_response_body(self):
+
+        response = requests.get(self.url)
+        return response.content
+
+    def parse_json(self):
+        return json.loads(self.get_response_body())
